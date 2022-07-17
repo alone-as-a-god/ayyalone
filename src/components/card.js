5007bbe87b31c384ae6694b8f97fb5f9705ed697
alone as a god <senoriship@gmail.com>
2022-07-17 18:15:56 +0200

    :)

--- src/components/card.js
+++ src/components/card.js
@@ -3,18 +3,16 @@ import React from "react";
 function Card(props){
     return(
     <a href={props.link} target="_blank" rel="noreferrer">
-    <div class="card bg-primary lg:min-w-full lg:min-h-full lg:h-72 m-5 lg:w-96 lg:bg-secondary shadow-xl m-0 ">
+    <div className="card bg-primary lg:min-w-full lg:min-h-full lg:h-72 m-5 lg:w-96 lg:bg-secondary shadow-xl m-0 ">
         <figure><img src={props.image} alt="" /></figure>
-        <div class="badge badge-secondary absolute top-0 right-0 mt-5 mr-5">NEW</div>
-        <div class="card-body p-6">
-            <h2 class="card-title"> {props.title}
+        <div className="badge badge-secondary absolute top-0 right-0 mt-5 mr-5">NEW</div>
+        <div className="card-body p-6">
+            <h2 className="card-title"> {props.title}
                 
             </h2>
-            {/* <p class="p-">{props.desc}</p> */}
-            <div class="card-actions absolute bottom-0 right-0 mb-5 mr-5">
-                {/* <a href={props.link} target="_blank" rel="noreferrer">
-                    <div class="group [transform:translateZ(0)] px-6 py-3 rounded-lg bg-gray-200 overflow-hidden relative before:absolute before:bg-sky-600 before:bottom-0 before:left-0 before:h-full before:w-full before:-translate-x-full hover:before:translate-x-0 before:transition before:ease-in-out before:duration-500"><span class="relative z-0 text-black group-hover:text-gray-200 transition ease-in-out duration-500">Go There!</span></div>
-                </a> */}
+           
+            <div className="card-actions absolute bottom-0 right-0 mb-5 mr-5">
+                
                
             </div>
         </div>
--- src/components/projects.js
+++ src/components/projects.js
@@ -3,14 +3,17 @@ import Card from './card';
 import "../index.css"
 
 function Projects() {
+
+
+
   return (
-    <div className="p-8 lg:p-24 mb-96">
-      <h1 class="text-7xl font-bold pb-12 textsheesh"> Check out some of my Projects </h1>
-      <div class="flex flex-wrap flex-col gap-4 w-full lg:flex-row">
-        <div class="grid  h-100 card bg-base-300 rounded-box place-items-center"><Card link="https://youtu.be/xE7jd9wRN-Y" title="24/7 Square Enix Lofi Livestream" desc="" image="https://i.ytimg.com/vi/wdokg_A8tOM/maxresdefault.jpg" /></div> 
-        <div class="grid  h-100 card bg-base-300 rounded-box place-items-center"><Card link="http://gzuz.ayyalone.lol" title="GZUZ Clicker" desc="" image="https://image.stern.de/9433132/t/Rb/v5/w1440/r1.7778/-/gzuz-aktuell.jpg" /></div>
-        <div class="grid  h-100 card bg-base-300 rounded-box place-items-center"><Card link="https://youtu.be/xE7jd9wRN-Y" title="24/7 Square Enix Lofi Livestream" desc="" image="https://i.ytimg.com/vi/wdokg_A8tOM/maxresdefault.jpg" /></div> 
-        <div class="grid  h-100 card bg-base-300 rounded-box place-items-center"><Card link="http://gzuz.ayyalone.lol" title="GZUZ Clicker" desc="" image="https://image.stern.de/9433132/t/Rb/v5/w1440/r1.7778/-/gzuz-aktuell.jpg" /></div>
+    <div className="p-8 lg:p-24 mb-80">
+      <h1 className="text-7xl font-bold pb-12 textsheesh"> Check out some of my Projects </h1>
+      <div className="flex flex-wrap flex-col gap-4 w-full lg:flex-row">
+        <div className="grid  h-100 card bg-base-300 rounded-box place-items-center"><Card link="https://youtu.be/xE7jd9wRN-Y" title="24/7 Square Enix Lofi Livestream" desc="" image="https://i.ytimg.com/vi/wdokg_A8tOM/maxresdefault.jpg" /></div> 
+        <div className="grid  h-100 card bg-base-300 rounded-box place-items-center"><Card link="http://gzuz.ayyalone.lol" title="GZUZ Clicker" desc="" image="https://image.stern.de/9433132/t/Rb/v5/w1440/r1.7778/-/gzuz-aktuell.jpg" /></div>
+        <div className="grid  h-100 card bg-base-300 rounded-box place-items-center"><Card link="https://youtu.be/xE7jd9wRN-Y" title="24/7 Square Enix Lofi Livestream" desc="" image="https://i.ytimg.com/vi/wdokg_A8tOM/maxresdefault.jpg" /></div> 
+        <div className="grid  h-100 card bg-base-300 rounded-box place-items-center"><Card link="http://gzuz.ayyalone.lol" title="GZUZ Clicker" desc="" image="https://image.stern.de/9433132/t/Rb/v5/w1440/r1.7778/-/gzuz-aktuell.jpg" /></div>
       </div>
         
         
